3a89497cdec3f9cbbde95d54cb3ada8b4b9694d5
Le Minh Thin <minhthinle69@gmail.com>
2025-06-11 22:50:59 +0700

    Got it to somewhat work

--- ex1/main.c
+++ ex1/main.c
@@ -5,17 +5,15 @@ typedef struct {
     int content[3];
 } State;
 
-const int CAPACITY[] = { 10, 7, 4 };
+const int CAPACITY[] = {10, 7, 4};
 
 Vector VISITED;
 
 int is_finishing_condition(State state) {
-    return state.content[2] == 2 || state.content[1] == 2;
+    return state.content[1] == 2 || state.content[2] == 2;
 }
 
-int min(int a, int b) {
-    return a < b ? a : b;
-}
+int min(int a, int b) { return a < b ? a : b; }
 
 int are_arrays_equal(int array1[], int array2[2], int length) {
     for (int i = 0; i < length; i++) {
@@ -28,7 +26,7 @@ int are_arrays_equal(int array1[], int array2[2], int length) {
 
 int has_visited(State state) {
     for (int i = 0; i < VISITED.size; i++) {
-        State current = *(State *) vector_get(&VISITED, i);
+        State current = *(State *)vector_get(&VISITED, i);
         if (are_arrays_equal(current.content, state.content, 3)) {
             return 1;
         }
@@ -37,16 +35,20 @@ int has_visited(State state) {
 }
 
 void find_moves(Vector *moves, State state, int checking) {
-    for (int i = 0; i < 3; i ++) {
+    for (int i = 0; i < 3; i++) {
         State current_move = state;
-        if (i == checking) continue;
-        if (current_move.content[i] == CAPACITY[i]) continue;
+        if (i == checking)
+            continue;
+        if (current_move.content[i] == CAPACITY[i])
+            continue;
 
-        int to_transfer = min(CAPACITY[i] - current_move.content[i], current_move.content[checking]);
+        int to_transfer = min(CAPACITY[i] - current_move.content[i],
+                              current_move.content[checking]);
         current_move.content[i] += to_transfer;
         current_move.content[checking] -= to_transfer;
 
-        if (has_visited(current_move)) continue;
+        if (has_visited(current_move))
+            continue;
 
         vector_push_back(moves, &current_move);
     }
@@ -54,7 +56,8 @@ void find_moves(Vector *moves, State state, int checking) {
 
 void get_valid_moves(Vector *moves, State state) {
     for (int i = 0; i < 3; i++) {
-        if (state.content[i] == 0) continue;
+        if (state.content[i] == 0)
+            continue;
         find_moves(moves, state, i);
     }
 }
@@ -65,6 +68,11 @@ void vector_extend(Vector *destination, Vector *source) {
     }
 }
 
+void print_root_and_node(State root, State node) {
+    printf("_%d_%d_%d_ -> _%d_%d_%d_\n", root.content[0], root.content[1],
+           root.content[2], node.content[0], node.content[1], node.content[2]);
+}
+
 void explore(State root) {
     Vector possible_moves;
     vector_setup(&possible_moves, 0, sizeof(State));
@@ -73,22 +81,26 @@ void explore(State root) {
     vector_extend(&VISITED, &possible_moves);
 
     for (int i = 0; i < possible_moves.size; i++) {
-        State node = *(State *) vector_get(&possible_moves, i);
-        printf("%d %d %d\n", node.content[0], node.content[1], node.content[2]);
+        State node = *(State *)vector_get(&possible_moves, i);
+        print_root_and_node(root, node);
         if (is_finishing_condition(node)) {
             return;
         }
-
         explore(node);
     }
 }
 
 int main() {
     vector_setup(&VISITED, 0, sizeof(State));
-    State root = (State) {
-        .content = { 0, 7, 4 }
-    };
+    State root = (State){.content = {0, 7, 4}};
+    vector_push_back(&VISITED, &root);
 
     explore(root);
+    for (int i = 0; i < VISITED.size; i++) {
+        State node = *(State *)vector_get(&VISITED, i);
+        printf("_%d_%d_%d_;\n", node.content[0], node.content[1],
+               node.content[2]);
+    }
+
     return 0;
 }
