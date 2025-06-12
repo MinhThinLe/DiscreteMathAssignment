e9a40b626e8975845ea470c0fd54dfce026642b1
Le Minh Thin <minhthinle69@gmail.com>
2025-06-12 14:50:41 +0700

    Added parsing dict into a graph

--- ex2/main.c
+++ ex2/main.c
@@ -1,34 +1,95 @@
 #include "vector.h"
 #include "stdio.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef char Word[7];
+// Should be correct on modern, 64bits computers
+#define POINTER_SIZE 8
 
+typedef struct {
+    char *content;
+    Vector nexts;
+} Node;
 
-void trim(Word word) {
-    word[5] = '\0';
+void trim(char buffer[]) {
+    buffer[5] = '\0';
 }
 
+// Read the dictionary, 
 void read_dict(Vector *words) {
     FILE *file = fopen("ex2/sgb-words.txt", "r");
 
-    Word line;
-    while (fgets(line, 7, file)) {
-        trim(line);
-        vector_push_back(words, line);
+    char *buffer = malloc(10);
+    while (fgets(buffer, 7, file)) {
+        trim(buffer);
+        vector_push_back(words, buffer);
+    }
+}
+
+void print_connections(Node *node) {
+    printf("Conneted to\n");
+    for (int i = 0; i < node->nexts.size; i++) {
+        Node *connected_node = (Node *) vector_get(&node->nexts, i);
+        printf("\t %s\n", connected_node->content);
     }
 }
 
 void vector_print(Vector *vector) {
     for (int i = 0; i < vector->size; i++) {
-        printf("%s\n", *(Word *) vector_get(vector, i));
+        Node *node = vector_get(vector, i);
+
+        printf("Node\ncontent: %s\n", node->content);
+        print_connections(node);
+    }
+}
+
+int are_words_connected(char *word1, char *word2) {
+    int differences = 0;
+    for (int i = 0; i < 5; i++) {
+        if (word1[i] != word2[i]) {
+            differences++;
+        }
+    }
+    return differences == 1;
+}
+
+void get_connected_nodes(Node *node, Vector *word_vector) {
+    for (int i = 0; i < word_vector->size; i++) {
+        char *word = vector_get(word_vector, i);
+        if (are_words_connected(node->content, word)) {
+            vector_push_back(&node->nexts, &word);
+        }
+    }
+}
+
+void construct_node_vector(Vector *node_vector, Vector *word_vector) {
+    for (int i = 0; i < word_vector->size; i++) {
+
+        Vector children;
+        vector_setup(&children, 0, POINTER_SIZE);
+
+        char *current_word = (char *) vector_get(word_vector, i);
+        Node current_node = {
+            .content = current_word,
+            .nexts = children,
+        };
+
+        get_connected_nodes(&current_node, word_vector);
+        vector_push_back(node_vector, &current_node);
     }
 }
 
 int main() {
     Vector words;
-    vector_setup(&words, 1000, sizeof(Word));
+    Vector node_vector;
+    char *pchar;
+
+    vector_setup(&words, 1000, sizeof(pchar));
+    vector_setup(&node_vector, 1000, sizeof(Node));
+
     read_dict(&words);
-    vector_print(&words);
+
+    construct_node_vector(&node_vector, &words);
+    vector_print(&node_vector);
     return 0;
 }
